53d19ffe2cd221f265fc81463b982528ac13c678
alicelee1231 <saemmilee1231@gmail.com>
2025-04-19 21:13:40 +0900

    Update router configuration for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
 import Game from "./components/Game";
 import "./App.css";
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/game" element={<Game />} />
-    </Routes>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Game />} />
+      </Routes>
+    </div>
   );
 };
 
